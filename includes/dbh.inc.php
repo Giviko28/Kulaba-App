e06706a999414d7ea4720d5674f175ff45eeb7d1
Giviko28 <chelidze.givia@gmail.com>
2023-05-31 16:32:31 +0400

    Added instructions for user

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,5 +1,8 @@
 <?php 
-
+/* Add your own personal servername, username, password.
+Make sure to completely remove the 3307 in the end (provided that your default port is 3306).
+Do not forget to import .sql file.
+*/
 $serverName = "localhost";
 $user = "root";
 $password = "Molekula28";
