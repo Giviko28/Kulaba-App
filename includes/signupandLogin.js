9e436398321126724834d13bc4ee6e1253849a26
Giviko28 <chelidze.givia@gmail.com>
2023-06-02 19:37:02 +0400

    client-side sign up with javascript + restyling

---
+++ includes/signupandLogin.js
@@ -0,0 +1,132 @@
+const signUpForm = document.querySelector("#signupForm");
+
+let symbolPattern = /[\W_]/;
+let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+function displayX(inp)  {
+    if(inp.indexOf("✓") !== -1) {
+        inp = inp.replace("✓", "x");
+    } else if (inp.indexOf("x") !== -1) {}
+    else {
+        inp += " x";
+    }
+    return inp;
+}
+function displayTick(inp) {
+    if(inp.indexOf("x") !== -1) {
+        inp = inp.replace("x", "✓");
+    } else if (inp.indexOf("✓") !== -1) {}
+    else  {
+        inp += " ✓";
+    }
+    return inp;
+}
+
+function buttonCheck() {
+    let button = document.querySelector("#submit");
+    let divSpan = document.querySelector(".messages");
+    let spans = divSpan.querySelectorAll("span");
+  
+    let tmp = 0;
+    for (let i = 0; i < spans.length; i++) {
+      let computedStyle = getComputedStyle(spans[i]);
+      let color = computedStyle.color.toLowerCase(); // Convert the color to lowercase
+  
+      if (color === "rgb(0, 128, 0)" || color === "green") {
+        tmp++;
+      }
+    }
+  
+    if (tmp === spans.length) {
+      button.className = "activeBtn";
+    } else {
+      button.className = "inActiveBtn";
+    }
+  }
+function validateName() {
+    let name = signUpForm["name"].value.trim();
+    let nameSpan = document.querySelector("#name");
+    if(name == "" || symbolPattern.test(name)){
+        nameSpan.style.color = "red";
+        nameSpan.innerText = displayX(nameSpan.innerText);
+        buttonCheck();
+        return false;
+    } else {
+        nameSpan.style.color = "green";
+        nameSpan.innerText = displayTick(nameSpan.innerText);
+    }
+    buttonCheck();
+    return true;
+}
+function validateEmail() {
+    let email = signUpForm["email"].value.trim();
+    let emailSpan = document.querySelector("#email");
+    if(email == "" || !emailPattern.test(email)){
+        emailSpan.style.color = "red";
+        emailSpan.innerText = displayX(emailSpan.innerText);
+        buttonCheck();
+        return false;
+    } else {
+        emailSpan.style.color = "green";
+        emailSpan.innerText = displayTick(emailSpan.innerText);
+    }
+    buttonCheck();
+    return true;
+}
+function validateUsername() {
+    let username = signUpForm["uid"].value.trim();
+    let usernameSpan = document.querySelector("#username");
+    if(username == "" || symbolPattern.test(username)) {
+        usernameSpan.style.color = "red";
+        usernameSpan.innerText = displayX(usernameSpan.innerText);
+        buttonCheck();
+        return false;
+    } else {
+        usernameSpan.style.color = "green";
+        usernameSpan.innerText = displayTick(usernameSpan.innerText);
+    }
+    buttonCheck();
+    return true;
+}
+
+function validatePwd() {
+    let pwd = signUpForm["pwd"].value.trim();
+    let pwdSpan = document.querySelector("#pwd");
+    if (pwd.length<8 || !/[A-Z]/.test(pwd) || !/[a-z]/.test(pwd) || !/[0-9]/.test(pwd)) {
+        pwdSpan.style.color = "red";
+        pwdSpan.innerText = displayX(pwdSpan.innerText);
+        buttonCheck();
+        return false;
+    } else {
+        pwdSpan.style.color = "green";
+        validatePwdRepeat();
+        pwdSpan.innerText = displayTick(pwd.innerText);
+    }
+    buttonCheck();
+    return true;
+}
+
+function validatePwdRepeat() {
+    let pwd = signUpForm["pwd"].value.trim();
+    let pwdRepeat = signUpForm["pwdrepeat"].value.trim();
+    let pwdRepeatSpan = document.querySelector("#pwdRepeat");
+    if(pwd !== pwdRepeat){
+        pwdRepeatSpan.style.color = "red";
+        pwdRepeatSpan.innerText = displayX(pwdRepeatSpan.innerText);
+        buttonCheck();
+        return false;
+    } else {
+        pwdRepeatSpan.style.color = "green";
+        pwdRepeatSpan.innerText = displayTick(pwdRepeatSpan.innerText);
+    }
+    buttonCheck();
+    return true;
+}
+
+function validateForm() {
+    if(validateName() && validateEmail() && validateUsername() && validatePwd() && validatePwdRepeat()){
+        return true;
+    }
+    alert("შეავსე ფორმა სწორად");
+    return false;
+}
--- signup.php
+++ signup.php
@@ -1,44 +1,51 @@
 <?php include "header.php" ?>
-    <section class="sign-up">
-        <h2>Sign Up</h2>
-        <form action="includes/signup.inc.php" method="post">
-            <input type="text" name="name" placeholder = "Name:">
-            <input type="text" name="email" placeholder = "Email:">
-            <input type="text" name="uid" placeholder = "Username:">
-            <input type="password" name="pwd" placeholder = "Password:">
-            <input type="password" name="pwdrepeat" placeholder = "Repeat your password:">
-            <button type="submit" name = "submit">Sign Up</button>
-        </form>
-    <?php 
-    if (isset($_GET["error"])) {
-        echo "<style>
-            p {
-                color: red;
-            }
-        </style>";
-        if ($_GET["error"] == "emptyinput") {
-            echo "<p>Fill in all field </p>";
-        }
-        else if ($_GET["error"] == "invaliduid") {
-            echo "<p>Invalid username</p>";
-        }
-        else if ($_GET["error"] == "invalidemail") {
-            echo "<p>Enter a valid email or username</p>";
-        }
-        else if ($_GET["error"] == "passworddsontmatch") {
-            echo "<p>The passwords don't match</p>";
-        }
-        else if ($_GET["error"] == "usernametaken") {
-            echo "<p>Email or Username already taken </p>";
-        }
-        else if ($_GET["error"] == "statementfailed") {
-            echo "<p>Something went wrong... Try again later</p>";
-        }
-        else if ($_GET["error"] == "none") {
-            echo "<p style = \"color: green;\">Succesfully signed up!</p>";
-        }
-    }
-    ?>
-    </section>
+<link rel="stylesheet" href="cssfolder/signup.css">
+    <main>
+        <div class = "box">
+            <h2>შემოგვიერთდით</h2>
+            <p>გაქვს უკვე ანგარიში? <a href="login.php">Log in</a></p>
+            <form id = "signupForm" action="includes/signup.inc.php" onsubmit = "return validateForm()" method="post">
+                <input onchange = "validateName()" type="text" name="name" placeholder = "სახელი:">
+                <input onchange = "validateEmail()" type="text" name="email" placeholder = "მეილი:">
+                <input onchange = "validateUsername()" type="text" name="uid" placeholder = "პირობითი სახელი:">
+                <input onchange = "validatePwd()" type="password" name="pwd" placeholder = "პაროლი:">
+                <input onchange = "validatePwdRepeat()" type="password" name="pwdrepeat" placeholder = "გაიმეორეთ პაროლი:">
+                <div class= "messages">
+                    <span id = "name">სახელი არ უნდა იყოს ცარიელი</span>
+                    <span id = "email">მეილის ფორმატი უნდა იყოს სწორი</span>
+                    <span id = "username">პირობითი სახელი არ უნდა იყოს ცარიელი</span>
+                    <span id = "pwd">პაროლი უნდა შეიცავდეს 8 სიმბოლოს, 1 დიდი, 1 პატარა და 1 ციფრი</span>
+                    <span id = "pwdRepeat">პაროლები უნდა ემთხვეოდეს</span>
+                </div>
+                <button id = "submit" class ="inActiveBtn" type="submit" name = "submit">რეგისტრაცია</button>
+            </form>
+            <?php 
+                if (isset($_GET["error"])) {
+                    if ($_GET["error"] == "emptyinput") {
+                        echo "<p>Fill in all field </p>";
+                    }
+                    else if ($_GET["error"] == "invaliduid") {
+                        echo "<p>Invalid username</p>";
+                    }
+                    else if ($_GET["error"] == "invalidemail") {
+                        echo "<p>Enter a valid email or username</p>";
+                    }
+                    else if ($_GET["error"] == "passworddsontmatch") {
+                        echo "<p>The passwords don't match</p>";
+                    }
+                    else if ($_GET["error"] == "usernametaken") {
+                        echo "<p>Email or Username already taken </p>";
+                    }
+                    else if ($_GET["error"] == "statementfailed") {
+                        echo "<p>Something went wrong... Try again later</p>";
+                    }
+                    else if ($_GET["error"] == "none") {
+                        echo "<p style = \"color: green;\">Succesfully signed up!</p>";
+                    }
+                }
+            ?>
+        </div>
 
+    </main>
+    <script src="includes/signupAndLogin.js"></script>
 <?php include "footer.php" ?>
